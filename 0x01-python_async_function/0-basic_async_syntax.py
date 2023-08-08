cd29af1b98100289464a1676a1d4beb2d860cfd9
sezfabian <cheruiyotfabian@gmail.com>
2023-08-09 02:49:54 +0300

    basic Async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
