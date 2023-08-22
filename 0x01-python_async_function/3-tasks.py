ccfe240213f77cee010a768b0ba452b22233c6d0
sezfabian <cheruiyotfabian@gmail.com>
2023-08-22 13:30:26 +0300

    task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+task_wait_random module
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int):
+    """
+    takes an integer max_delay and returns a asyncio.Task.
+    """
+
+    return asyncio.create_task(wait_random(max_delay))
+
